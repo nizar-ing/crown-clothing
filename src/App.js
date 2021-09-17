5f27b21422146ac50ffb4f28c967107abf525e6f
nizar-ing <nizar.ilahi@gmail.com>
2021-09-17 16:11:21 +0100

    broken our homepage into smaller components and styling it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 import "./App.css";
-import "./homepage.styles.scss";
 
 function App() {
   return (
