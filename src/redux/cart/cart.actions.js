1ced2ce80439683ee6841f5906a8f3ea882742da
nizar-ing <nizar.ilahi@gmail.com>
2021-09-29 22:04:42 +0100

    updated shop dara with new data, updated cart utils to group items into the cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
-import CartActionsTypes from "./cart.types";
+import CartActionsTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
   type: CartActionsTypes.TOGGLE_CART_HIDDEN,
+});
+
+export const addItem = (item) => ({
+  type: CartActionsTypes.ADD_ITEM,
+  payload: item,
 });
